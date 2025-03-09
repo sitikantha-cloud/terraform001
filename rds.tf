1457c7af158143322ae8c350f432b81214544d7f
sitikantha-cloud <sitikantha.rath333@gmail.com>
2025-03-09 17:05:18 +0530

    Staging (#3)
    * add backend.tf
    
    * modified db intance type

--- rds.tf
+++ rds.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 5
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  engine_version       = "8.0.40"
+  instance_class       = "db.t4g.micro"
   db_name                 = "mydb"
   username             = "foo"
   password             = file("../rds_pass.txt")
